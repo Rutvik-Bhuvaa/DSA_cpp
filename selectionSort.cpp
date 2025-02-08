71e6b9c36f7ffb3594dbe762e1b2813884373d4d
RutvikBhuvaa <rutvikbhuvaa9@gmail.com>
2025-02-08 10:58:49 +0530

    selction  sort algo modifies

--- selectionSort.cpp
+++ selectionSort.cpp
@@ -3,28 +3,30 @@ using namespace std;
 
 void selectionSort(int arr[], int size){
     for(int i=0; i<size-1; i++){
-        int index = i;
-        for(int j=i; j<size; j++){
-            if(arr[j] < arr[index]){
-                index = j;
+        int minIndex = i;
+        for(int j=i+1; j<size; j++){
+            if(arr[j] < arr[minIndex]){
+                minIndex = j;
             }
         }
-        int temp = arr[index];
-        arr[index] = arr[i];
+        int temp = arr[minIndex];
+        arr[minIndex] = arr[i];
         arr[i] = temp;
     }
-    
-    cout << "Printing the array" << endl;
+}
+
+void printingArray(int arr[], int size){
     for(int i=0; i<size; i++){
         cout << arr[i] << " ";
     }
- 
 }
 
 int main(){
     int arr[5] = {4, 1, 3, 9, 7};
     int size = (sizeof arr/sizeof arr[0]);
-
+    
     selectionSort(arr, size);
+    cout << "Sorted array is: ";
+    printingArray(arr, size);
     return 0;
 }
