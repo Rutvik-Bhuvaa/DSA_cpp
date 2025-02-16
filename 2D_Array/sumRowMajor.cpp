4efa17e0ceb652d485e3a57c0fa470707bb80e30
RutvikBhuvaa <rutvikbhuvaa9@gmail.com>
2025-02-16 07:55:14 +0530

    Sum in Row major order

---
+++ 2D_Array/sumRowMajor.cpp
@@ -0,0 +1,34 @@
+// Find any element in 2D array
+
+#include<iostream>
+using namespace std;
+
+int sumRowMajor(int matrix[3][3], int row, int col){
+    int sum = 0;
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+            sum += matrix[i][j];
+        }
+        cout <<"Sum of till row " << i << " " << sum << endl;
+    }
+
+    return sum;
+}
+
+int main(){
+
+    int row = 3;
+    int col = 3;
+
+    int matrix[3][3];
+    cout << "Enter the no in 2D Array: " << endl;
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+            cin >> matrix[i][j];
+        }
+    }
+
+    int sum = sumRowMajor(matrix, row, col);
+    cout << "Total sum is: " << sum;
+    return 0;
+}
