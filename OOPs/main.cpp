c85b7651fe5987b0453995c3bc961483f5b963bb
RutvikBhuvaa <rutvikbhuvaa9@gmail.com>
2025-06-08 10:00:16 +0530

    Added some other stuff

--- OOPs/main.cpp
+++ OOPs/main.cpp
@@ -10,9 +10,14 @@ class Student{
 };
 
 class Bank{
+    // note: never write states using public access modifier if possible and use member functions to access this states
     public:
     int balance;
     string name;
+
+    void setValue(int amount){
+        balance += amount;
+    }
 
 
     void checkBalance(){
@@ -22,6 +27,35 @@ class Bank{
     void withdraw(){
         balance-=100;
         cout << "100 rupees withdrawn successfully" << endl;
+    }
+};
+
+class Car {
+    int mileage; 
+    int cost;
+    // char c;
+
+    public:
+    // Basic constructor
+    Car(){
+        mileage = 10;
+        cost = 200000;
+        cout << "Hello constructor"<< endl;
+    }
+
+    // Parameterized constructor
+    Car(int mileage, int cost){
+        this->mileage = mileage;
+        this->cost = cost;
+        cout << "Parameterized constructor called" << endl;
+    }
+
+    void print(){
+        cout << cost << " " << mileage << endl;
+    }
+
+    ~Car(){
+        cout << "Destructor executed";
     }
 };
 
@@ -40,5 +74,13 @@ int main(){
     customer.checkBalance();
     customer.withdraw();
     customer.checkBalance();
+    customer.setValue(20000);
+    customer.checkBalance();
+
+    Car bmw;
+    cout << sizeof(bmw) << endl;
+    Car rangerover(10, 300000000);
+    bmw.print();
+    rangerover.print();
     return 0;
 }
