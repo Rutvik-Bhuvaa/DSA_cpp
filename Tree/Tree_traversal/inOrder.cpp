26ce3fc421ab1d2cdad6ed595d3835257c884cca
RutvikBhuvaa <rutvikbhuvaa9@gmail.com>
2025-06-23 14:51:50 +0530

    inorder tree traversal

---
+++ Tree/Tree_traversal/inOrder.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int value){
+        data = value;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+Node *binaryTree(){
+    int x;
+    cout << "Enter the value: " << endl;
+    cin >> x;
+
+    if(x == -1){
+        return nullptr;
+    }
+
+    Node *root = new Node(x);
+    cout << "Enter the left child of " << x << endl;
+    root->left = binaryTree();
+    cout << "Enter the right child of " << x << endl;
+    root->right = binaryTree();
+
+    return root;
+}
+
+void inOrder(Node *root){
+    if(!root){
+        return;
+    }
+
+    inOrder(root->left);
+    cout << root->data << " ";
+    inOrder(root->right);
+}
+
+int main(){
+    Node *root = binaryTree();
+
+    inOrder(root);
+    
+    return 0;
+}
